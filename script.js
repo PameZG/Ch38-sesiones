b1cadef338f9d4a8cbc7c10a755282c622e6935f
PameZG <pamelaz.g.@Pamelas-MacBook-Pro.local>
2024-02-19 13:20:05 -0600

    ci: funcion saludar a persona

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Hola Ch38");
+
+// Función que envie un saludo con alert.
+function enviarSaludo(){
+    alert("¡Hola Ch38!");
+}
+
+/**
+ * Pedir el nombre de la persona usando un promt()
+ * Mostrar un saludo y nombre de la persona con Alert()
+ */
+function enviarSaludoAPersona(){
+let message = prompt ("¿Cómo te llamas?");
+alert(`Hola ${message}`);
+}
